0078be1c94177395e68171765f642be68db31c4a
Arin Jason <67254637+ArinJason@users.noreply.github.com>
2026-01-22 00:47:47 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// DB Connection
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/auth.routes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
